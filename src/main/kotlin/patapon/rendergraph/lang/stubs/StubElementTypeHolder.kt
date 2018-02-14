cdb4ec86fa235cba0cf56fd637304f319736de08
Alexandre Bléron <alex.bleron@gmail.com>
2018-02-14 13:17:57 +0100

    fghdgdxgf

---
+++ src/main/kotlin/patapon/rendergraph/lang/stubs/StubElementTypeHolder.kt
@@ -0,0 +1,9 @@
+package patapon.rendergraph.lang.stubs
+
+import com.intellij.psi.stubs.IStubElementType
+
+interface StubElementTypeHolder {
+    companion object {
+        val COMPONENT = RgComponentStub.Type
+    }
+}
